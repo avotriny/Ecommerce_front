2df52792c6bd17cc10905f29d1f74c4cbe5088b1
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-06 15:03:49 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,18 @@
-import App from './App';
-import React, { Suspense } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import './i18n';
-const LoadingScreen = () => (
-  <div className="flex items-center justify-center h-screen">
-    <div className="animate-spin rounded-full h-16 w-16 border-4 border-blue-500"></div>
-  </div>
-);
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-<Suspense fallback={<LoadingScreen />}>
-    <App />
-  </Suspense>
-  </BrowserRouter>,
-    
+import App from './App';
+import { CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import ContextProvider from './context/ContextProvider';
+import './index.css';
 
+createRoot(document.getElementById('root')).render(
+  <>
+    <ContextProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ContextProvider>
+  </>
 );
