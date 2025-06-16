45041b12b10db7bfaa3d4d3d92e8a7e97ad1a646
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-16 18:21:49 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,34 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loading from './Loading';
 import Notification from './Notification';
-import axios from 'axios'; 
-import Navbar from './Navbar/Navbar'
 import PageTransition from './utils/PageTransition';
-import Accueil from './components/Accueil/accueil'
-import Categorie from './Dashboard/Categorie/Categorie';
+import Client from './Client';
+import Dashboard from './Dashboard/Dashboard';
+import { NotFoundPage } from './NotFoundPage';
+import { ForbiddenPage } from './ForbiddenPage';
+import { UnauthorizedPage } from './UnauthorizedPage';
+import Login from './Auth/Auth';
+import RegistrationVerify from './Auth/RegistrationVerify';
+import RegistrationConfirm from './Auth/RegistrationConfirm';
 
-
-axios.defaults.baseURL = 'http://localhost:8000/';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.post['Accept'] = 'application/json';
-axios.defaults.withCredentials = true;
-axios.interceptors.request.use(function(config) {  
-    const token = localStorage.getItem('auth_token'); 
-    config.headers.Authorization = token ? `Bearer ${token}` : '';
-    return config;
-});
-
-const App = () => {
+export default function App() {
   return (
-<Router>
-      <Navbar/>
-      
+    <Router>
       <Loading />
       <Notification />
       <PageTransition>
-      <Routes>
-      <Route path="/" element={<Accueil/>} />
-        <Route path="/categorie" element={<Categorie />} />
-        
-        {/* <Route path="/shop" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Auth />} />
-        <Route path="*" element={<Page404 />} />
-        <Route element={<AdminPrivateRoutes />} >
-        <Route path="/dashboard/*" element={<Dashboard />} /> */}
-        {/* </Route> */}
-
-      </Routes>
+        <Routes>
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="/*" element={<Client />} />
+          <Route path="/401" element={<UnauthorizedPage />} />
+          <Route path="/403" element={<ForbiddenPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/auth" element={<Login />} />
+          <Route path="/registration/verify" element={<RegistrationVerify />} />
+          <Route path="/registration/confirm" element={<RegistrationConfirm />} />
+        </Routes>
       </PageTransition>
-      </Router>
-      
-
+    </Router>
   );
-};
-
-export default App;
+}
