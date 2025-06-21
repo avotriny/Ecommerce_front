74466043d7c0fe53a36a357b01b704ef0a191002
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-21 08:10:18 +0200

    Config CORS

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,17 @@ import { UnauthorizedPage } from './UnauthorizedPage';
 import Login from './Auth/Auth';
 import RegistrationVerify from './Auth/RegistrationVerify';
 import RegistrationConfirm from './Auth/RegistrationConfirm';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function(config) {  
+    const token = localStorage.getItem('auth_token'); 
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+});
 
 export default function App() {
   return (
