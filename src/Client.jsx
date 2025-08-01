ecb1219ca11b074d4c1e075f2ed0c6c7580e6a15
Avotriniaina <avotriniainajonatan@gmail.com>
2025-08-01 07:06:03 +0200

    ajout path produit detail et list produit

--- src/Client.jsx
+++ src/Client.jsx
@@ -6,6 +6,8 @@ import Accueil from './components/Accueil/accueil';
 import Shop from './components/Shop/Shop';
 import Cart from './components/Cart/Cart';
 import OrdersMap from './components/Carte/Carte';
+import ProduitDetail from './components/Produit/ProduitDetail';
+import ListProduit from './Dashboard/Produit/ListProduit';
 
 const Client = () => {
   return (
@@ -13,7 +15,10 @@ const Client = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Accueil />} />
+        <Route path="/produit/:nom" element={<ProduitDetail />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="/produit" element={<ListProduit />} />
+
         <Route path="cart" element={<Cart />} />
         <Route path="carte" element={<OrdersMap />} />
       </Routes>
